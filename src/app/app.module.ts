8bbee14f7fc50c3b105e288e21366971e22c12c2
Sergiy Gerasimov <jamsg.program@gmail.com>
2022-07-21 20:01:46 +0300

    second component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {HeaderComponent} from "./header/header.component";
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
